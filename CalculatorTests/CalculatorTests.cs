9c9aa2933e68c551919d04c944f79b5c097988d5
trainingby1 <trainingby1@google.com>
2022-10-03 22:11:51 +0300

    add unit test

---
+++ CalculatorTests/CalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void parseStatementTest()
+        {
+            string statement = "1+1";
+            Assert.AreEqual("2", Calculator.parseStatement(statement));
+        }
+    }
+}
