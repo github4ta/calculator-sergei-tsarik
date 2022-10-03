0d1384c8365f1a46833bbec32cac6279cd4717cd
trainingby1 <trainingby1@google.com>
2022-10-03 22:11:30 +0300

    refactor form

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public static string parseStatement(string statement)
+        {
+            int a = int.Parse(statement.Substring(0, 1));
+            int b = int.Parse(statement.Substring(2, 1));
+            // string operation = statement.Substring(1, 1);
+            int result = a + b;
+            return result.ToString();
+        }
+    }
+}
---
+++ Calculator/FormCalculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class FormCalculator : Form
+    {
+        public FormCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOne_Click(object sender, EventArgs e)
+        {
+            textDisplay.Text = textDisplay.Text + "1";
+        }
+
+        private void buttonTwo_Click(object sender, EventArgs e)
+        {
+            textDisplay.Text = textDisplay.Text + "2";
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            textDisplay.Text = textDisplay.Text + "+";
+        }
+
+        private void buttonResult_Click(object sender, EventArgs e)
+        {
+            textDisplay.Text = Calculator.parseStatement(textDisplay.Text);
+        }
+    }
+}
